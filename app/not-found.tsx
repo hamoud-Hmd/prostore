f0b48a6005d32e80adcdaae093d47dbe3357cb2e
hamoud-hmd <hamoudeded@gmail.com>
2025-12-30 18:29:20 +0100

    Add loading page and not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+'use client';
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+const NotFoundPage = () => {
+    return (<div className="flex flex-col items-center justify-center min-h-screen">
+        <Image src='/images/logo.svg' alt={`${APP_NAME} Logo`} height={48} width={48} priority={true} />
+        <div className="p-6 rounded-lg shadow-md text-center">
+            <h1 className="text-3xl font-bold mb-4">Page Not Found</h1>
+            <p className="text-destructive">The page you are looking for does not exist.</p>
+            <Button variant="outline" className="mt-4 ml-2"
+            onClick={() => (window.location.href = '/')}>Go Home</Button>
+        </div>
+    </div>);
+}
+
+export default NotFoundPage;
