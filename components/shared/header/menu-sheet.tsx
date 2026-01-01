5bb53e100e1890944a1944545f57f7a163d0b099
hamoud-hmd <hamoudeded@gmail.com>
2026-01-01 02:57:58 +0100

    Seperate menu sheet to its own component

---
+++ components/shared/header/menu-sheet.tsx
@@ -0,0 +1,37 @@
+import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react";
+import { SheetTrigger, Sheet, SheetContent, SheetTitle, SheetDescription} from "@/components/ui/sheet";
+import ModeToggle from "./mode-toggle";
+import { Button } from "@/components/ui/button";
+import Link from 'next/link';
+
+
+const MenuSheet = () => {
+    return (
+        <nav className="md:hidden">
+            <Sheet>
+
+                <SheetTrigger className="align-middle" >
+                    <EllipsisVertical />
+                </SheetTrigger>
+                <SheetContent className="flex flex-col items-start">
+                    <SheetTitle>Menu</SheetTitle>
+                    <ModeToggle />
+                    <Button asChild variant="ghost" className="w-full mt-2">
+                        <Link href="/cart" >
+                            <ShoppingCart /> Cart
+                        </Link>
+                    </Button>
+                    <Button asChild className="w-full mt-2">
+                        <Link href="/sign-in" >
+                            <UserIcon /> Sign In
+                        </Link>
+                    </Button>
+                    <SheetDescription></SheetDescription>
+                </SheetContent>
+
+            </Sheet>
+        </nav>
+    );
+}
+
+export default MenuSheet;
